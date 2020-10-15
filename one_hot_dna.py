776d7775d2d5cfb24b5b1fc223165cb216667e6c
JBWDN <jake.bowden95@gmail.com>
2020-10-15 12:25:50 +0100

    working model

--- one_hot_dna.py
+++ one_hot_dna.py
@@ -1,3 +1,4 @@
+import numpy as np
 from typing import List
 
 encode_dict = {
@@ -5,16 +6,20 @@ encode_dict = {
     "T": [0, 1, 0, 0],
     "C": [0, 0, 1, 0],
     "G": [0, 0, 0, 1],
+    "X": [0, 0, 0, 0]
 }
 
 
-def one_hot_dna(input_seq_str: str) -> List[list]:
+def one_hot_dna(input_seq_str: str, max_seq_len: int) -> List[list]:
     """
     One hot encode a string format dna sequence. 
+    Add zero padding up to the maximum length.
     """
     input_seq_upper = input_seq_str.upper()
-    encoded_dna = [encode_dict[base] for base in input_seq_upper]
-    return encoded_dna
+    padding = "".join(["X" for i in range(max_seq_len - len(input_seq_str))])
+    encoded_dna = [encode_dict[base] for base in input_seq_upper + padding]
+    np_encoded = np.array(encoded_dna)
+    return np_encoded.flatten()
 
 
 if __name__ == "__main__":
--- train_model.py
+++ train_model.py
@@ -1,16 +1,51 @@
-import pandas
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
 import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+print(tf.__version__)
 
 from one_hot_dna import one_hot_dna
 
-
 if __name__ == "__main__":
-    # Load training data and separate into input (X) and output (Y):
-    df = pandas.read_csv("training_data.csv", header=None)
+    # Load training data and separate into features (X) and labels (Y):
+    df = pd.read_csv("training_data.csv", header=None)
     dataset = df.values
 
-    X_seq = dataset[:,0]
-    X = [one_hot_dna(seq) for seq in X_seq]
-    Y = dataset[:,1]
-    
+    X_seq = dataset[:, 0]
+    # One hot ecode primer sequences...
+    X = np.array([one_hot_dna(seq, 50) for seq in X_seq], dtype=int)
+    Y = np.array(dataset[:, 1], dtype="float64")
+
+    X_train = X[:4000]
+    X_test = X[4000:]
+    X_test_seq = X_seq[4000:]
+    Y_train = Y[:4000]
+    Y_test = Y[4000:]
+
+    # Define the model (200 input node, 1 fully connected hidden layer, output):
+    tm_model = keras.Sequential([
+            layers.Dense(200, activation="relu"),
+            layers.Dense(64, activation="relu"),
+            layers.Dense(1),
+        ])
+    tm_model.compile(loss="mean_absolute_error", optimizer=keras.optimizers.Adam(0.001))
+
+    # Train model:
+    tm_model.fit(X_train, Y_train, epochs=100)
+
+    # Validate the model against the training set:
+    print("Validating model...")
+    predictions = tm_model.predict(X_test)
+
+    print("Example results:")
+    for i in range(len(predictions)):
+        print(
+            f"{i}: model={float(predictions[i])}, real={Y_test[i]}, diff={abs(float(predictions[i]) - Y_test[i])},sequence={X_test_seq[i]}"
+        )
+        if i == 20:
+            break
     pass
